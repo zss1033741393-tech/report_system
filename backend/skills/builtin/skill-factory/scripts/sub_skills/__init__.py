c5f96caf6f1e1dc79f37afc37869d08ecb9ea904
zss1033741393-tech <zss1033741393@gmail.com>
2026-03-27 19:18:22 +0800

    Create __init__.py

---
+++ backend/skills/builtin/skill-factory/scripts/sub_skills/__init__.py
@@ -0,0 +1,8 @@
+from .intent_understand import IntentUnderstand
+from .struct_extract import StructExtract
+from .outline_design import OutlineDesign
+from .data_binding import DataBinding
+from .report_preview import ReportPreview
+from .skill_persist import SkillPersist
+
+ALL_SUB_SKILLS = [IntentUnderstand, StructExtract, OutlineDesign, DataBinding, ReportPreview, SkillPersist]
